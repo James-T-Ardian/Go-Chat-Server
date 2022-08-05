0b856b82f6fbb29521807fbe60940e9a35fef5ce
James T Ardian <jamesardian01@gmail.com>
2022-08-04 21:10:05 -0700

    add omit empty for optional Message fields

--- pkg/websocket/message.go
+++ pkg/websocket/message.go
@@ -6,8 +6,8 @@ const LeaveRoom = "leave-room"
 
 type Message struct {
 	Action    string `json:"action"`
-	Timestamp string `json:"timeStamp"`
-	Body      string `json:"body"`
+	Timestamp string `json:"timeStamp,omitempty"`
+	Body      string `json:"body,omitempty"`
 	Sender    string `json:"sender,omitempty"`
 	Target    string `json:"target"`
 }
